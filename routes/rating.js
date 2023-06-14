61ff31227c0e73c5cc4fe31763afcef51c11c2eb
Aldyy123 <mohammad.aldyy@gmail.com>
2023-06-14 13:59:12 +0700

    change method put rating

--- routes/rating.js
+++ routes/rating.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 
 
 router.route('/lesson/:lesson_id').post(authentication, insertRating)
-router.route('/id/:rating_id').post(authentication, updateRating)
+router.route('/id/:rating_id').put(authentication, updateRating)
 router.route('/lesson/:lesson_id/nidn/:nidn').get(authentication, getAllRatingLessonWithLecturer)
 // router.route('/id/:id').get(authentication, getLessonById).put(authentication, authorization, updateLesson).delete(authentication, authorization, deleteLesson)
 
